91a1d50e9c3ed2b02368b22de6ddaa2acc648b2b
Administrator <453055044@qq.com>
2019-09-10 18:08:52 +0800

    come on

--- src/main/java/com/miaosha2/miaosha2/util/MD5Util.java
+++ src/main/java/com/miaosha2/miaosha2/util/MD5Util.java
@@ -32,12 +32,8 @@ public class MD5Util {
     }
 
     public static void main(String[] args) {
-        // 12123456c3
-        // d3b1294a61a07da9b49b6e22b2cbd7f9d3b1294a61a07da9b49b6e22b2cbd7f9
         System.out.println(inputPassToFormPass("123456"));
         System.out.println(formPassToDBPass(inputPassToFormPass("123456"), "1a2b3c4d"));
-
-        // b7797cce01b4b131b433b6acf4add449
         System.out.println(inputPassToDbPass("123456", "1a2b3c4d"));
     }
 }
